feb5a5d057e94e747054af59ddc797f1b8f8ecf1
Mohammed Hussain <jakubarshathikmohammedhussain@gmail.com>
2026-09-20 22:35:10 +0530

    Create layout.js

---
+++ app/layout.js
@@ -0,0 +1,10 @@
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body style={{ margin: 0, backgroundColor: 'black' }}>
+        {children}
+      </body>
+    </html>
+  );
+}
+
